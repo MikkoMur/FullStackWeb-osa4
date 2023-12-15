67cff9213eb47726bd2ee4472e8ea7fcf5d13173
Mikko Murhu <mikko.murhu@aalto.fi>
2023-12-15 11:01:51 +0200

    Assignments 4.13-4.14

--- tests/api.test.js
+++ tests/api.test.js
@@ -117,104 +117,50 @@ test('if a new blog is missing a url, return bad request status 400', async () =
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 })
 
-
-// test('notes are returned as json', async () => {
-//   await api
-//     .get('/api/notes')
-//     .expect(200)
-//     .expect('Content-Type', /application\/json/)
-// })
-
-// test('all notes are returned', async () => {
-//   const response = await api.get('/api/notes')
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
 
 
-//   expect(response.body).toHaveLength(helper.initialNotes.length)
-// })
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
 
-// test('a specific note is within the returned notes', async () => {
-//   const response = await api.get('/api/notes')
+  const blogsAtEnd = await helper.blogsInDb()
 
-//   const contents = response.body.map(r => r.content)
-//   expect(contents).toContain(
-//     'Browser can execute only JavaScript'
-//   )
-// })
+  expect(blogsAtEnd).toHaveLength(
+    helper.initialBlogs.length - 1
+  )
 
-// test('a valid note can be added ', async () => {
-//   const newNote = {
-//     content: 'async/await simplifies making async calls',
-//     important: true,
-//   }
+  const titles = blogsAtEnd.map(b => b.title)
 
-//   await api
-//     .post('/api/notes')
-//     .send(newNote)
-//     .expect(201)
-//     .expect('Content-Type', /application\/json/)
+  expect(titles).not.toContain(blogToDelete.title)
+})
 
+test('the amount of likes in a blog can be modified', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToModify = blogsAtStart[0]
+  const initialLikes = blogToModify.likes
 
+  const modifiedBlog = {
+    title: blogToModify.title,
+    aithor: blogToModify.author,
+    url: blogToModify.url,
+    likes: blogToModify.likes + 1
+  }
 
-//   const notesAtEnd = await helper.notesInDb()
-//   expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
+  await api
+    .put(`/api/blogs/${blogToModify.id}`)
+    .send(modifiedBlog)
+    .expect(200)
 
-
-//   const contents = notesAtEnd.map(n => n.content)
-//   expect(contents).toContain(
-//     'async/await simplifies making async calls'
-//   )
-// })
-
-// test('note without content is not added', async () => {
-//   const newNote = {
-//     important: true
-//   }
-
-//   await api
-//     .post('/api/notes')
-//     .send(newNote)
-//     .expect(400)
-
-
-//   const notesAtEnd = await helper.notesInDb()
-
-
-//   expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
-// })
-
-// test('a specific note can be viewed', async () => {
-//   const notesAtStart = await helper.notesInDb()
-
-//   const noteToView = notesAtStart[0]
-
-
-//   const resultNote = await api
-//     .get(`/api/notes/${noteToView.id}`)
-//     .expect(200)
-//     .expect('Content-Type', /application\/json/)
-
-//   expect(resultNote.body).toEqual(noteToView)
-// })
-
-// test('a note can be deleted', async () => {
-//   const notesAtStart = await helper.notesInDb()
-//   const noteToDelete = notesAtStart[0]
-
-
-//   await api
-//     .delete(`/api/notes/${noteToDelete.id}`)
-//     .expect(204)
-
-//   const notesAtEnd = await helper.notesInDb()
-
-//   expect(notesAtEnd).toHaveLength(
-//     helper.initialNotes.length - 1
-//   )
-
-//   const contents = notesAtEnd.map(r => r.content)
-
-//   expect(contents).not.toContain(noteToDelete.content)
-// })
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(
+    helper.initialBlogs.length
+  )
+  const likesAfter = blogsAtEnd[0].likes
+  expect(likesAfter).toEqual(initialLikes + 1)
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
--- tests/api_test_helper.js
+++ tests/api_test_helper.js
@@ -20,19 +20,6 @@ const blogsInDb = async () => {
   return notes.map(note => note.toJSON())
 }
 
-// const nonExistingId = async () => {
-//   const note = new Note({ content: 'willremovethissoon' })
-//   await note.save()
-//   await note.remove()
-
-//   return note._id.toString()
-// }
-
-// const notesInDb = async () => {
-//   const notes = await Note.find({})
-//   return notes.map(note => note.toJSON())
-// }
-
 module.exports = {
-  initialBlogs, blogsInDb //, nonExistingId, notesInDb
+  initialBlogs, blogsInDb
 }
